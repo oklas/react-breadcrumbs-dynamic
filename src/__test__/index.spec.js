5bd2d10d7e0f191db5dd9c2fe657d6d771aafa24
Serguei Okladnikov <oklaspec@gmail.com>
2017-08-22 18:53:05 +0300

    test replace item with new value of prop `to`

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -5,33 +5,49 @@ import { expect } from 'chai'
 import {
   BreadcrumbsProvider,
   Breadcrumbs,
-  BreadcrumbsItem
+  BreadcrumbsItem,
+  withBreadcrumbs,
 } from '../index';
 
 
 jest.dontMock('../index')
 
-function buildDom() {
-  return (
-    <BreadcrumbsProvider>
-      <div>
-        <Breadcrumbs />
-        <BreadcrumbsItem to='/'>Home</BreadcrumbsItem>
-        <BreadcrumbsItem to='/user'>User</BreadcrumbsItem>
-        <BreadcrumbsItem to='/user/profile'>Profile</BreadcrumbsItem>
-      </div>
-    </BreadcrumbsProvider>
-  )
+class TestSimpleApp extends React.Component {
+  state = {
+    profileUrl: "/user/profile"
+  }
+  render() {
+    return (
+      <BreadcrumbsProvider>
+        <div>
+          <Breadcrumbs />
+          <BreadcrumbsItem to='/'>Home</BreadcrumbsItem>
+          <BreadcrumbsItem to='/user'>User</BreadcrumbsItem>
+          <BreadcrumbsItem to={this.state.profileUrl}>Profile</BreadcrumbsItem>
+          <button onClick={()=>this.setState({profileUrl:"/user/settings"})}>
+            Change Profile Url
+          </button>
+        </div>
+      </BreadcrumbsProvider>
+    )
+  }
 }
 
 describe("breadcrumbs simple usage", function() {
   it("dom rendering", function() {
     jest.useFakeTimers()
-    const wrapper = mount(buildDom());
+    const wrapper = mount(<TestSimpleApp/>);
     jest.runAllTimers();
     expect(wrapper.find('a')).to.have.length(3);
     expect(wrapper.find('a').at(0).props().to).to.equal('/')
     expect(wrapper.find('a').at(1).props().to).to.equal('/user')
     expect(wrapper.find('a').at(2).props().to).to.equal('/user/profile')
+    wrapper.find('button').simulate('click');
+    jest.runAllTimers();
+    expect(wrapper.find('a')).to.have.length(3);
+    expect(wrapper.find('a').at(0).props().to).to.equal('/')
+    expect(wrapper.find('a').at(1).props().to).to.equal('/user')
+    expect(wrapper.find('a').at(2).props().to).to.equal('/user/settings')
   });
 });
+
