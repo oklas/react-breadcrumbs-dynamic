ed99c5740e17466f293e99f155ad34fc421d1dc8
Serguei Okladnikov <oklaspec@gmail.com>
2018-01-05 19:34:33 +0300

    test replace another (by react-router) with same props #12

---
+++ src/__test__/router.spec.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import enzyme, { shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-15';
+import { expect } from 'chai'
+
+import {
+  BreadcrumbsProvider,
+  Breadcrumbs,
+  BreadcrumbsItem,
+} from '../index'
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
+
+enzyme.configure({ adapter: new Adapter() });
+jest.dontMock('../index')
+
+const runAllTimers = () => jest.runAllTimers()
+const useFakeTimers = () => jest.useFakeTimers()
+
+
+const createComponent = (children) => {
+  const SameComponent = () => (
+    <div>
+      <BreadcrumbsItem to='/same'><i>{children}</i></BreadcrumbsItem>
+    </div>
+  )
+  return SameComponent
+}
+
+
+class TestApp extends React.Component {
+
+  render() {
+    return (
+      <Router>
+        <BreadcrumbsProvider>
+          <div>
+
+            <Breadcrumbs/>
+
+            <Route
+              render={({history}) => (
+                <div>
+                  <button
+                    type='button'
+                    className="navigateToIndex"
+                    onClick={() => { history.push('/same') }}
+                    children='index'
+                  />
+
+                  <button
+                    type='button'
+                    className="navigateToSame"
+                    onClick={() => { history.push('/same/1') }}
+                    children='same'
+                  />
+                </div>
+              )}
+            />
+
+            <Route exact path="/same" component={createComponent('index')} />
+            <Route path="/same/:id" component={createComponent('same')} />
+
+          </div>
+        </BreadcrumbsProvider>
+      </Router>
+    )
+  }
+}
+
+
+describe('breadcrumbs with react-router', function() {
+  it("replace another element with same props", function() {
+    useFakeTimers()
+    const wrapper = mount(<TestApp/>)
+    runAllTimers()
+
+    wrapper.find('.navigateToIndex').simulate('click')
+    expect(wrapper.find('a').at(0).props().to).to.equal('/same')
+    expect(wrapper.find('i').at(0).props().children).to.equal('index')
+
+    wrapper.find('.navigateToSame').simulate('click')
+    expect(wrapper.find('a').at(0).props().to).to.equal('/same')
+    expect(wrapper.find('i').at(0).props().children).to.equal('same')
+
+    wrapper.unmount()
+  })
+})
